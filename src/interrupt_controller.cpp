dd492db0a0203e9e97b86b60ff06cd16c9b475e5
Rob <robbie.mcl@icloud.com>
2026-07-01 12:15:19 -0500

    Add comments to interrupt controller regarding our asserts over hardware

--- src/interrupt_controller.cpp
+++ src/interrupt_controller.cpp
@@ -4,6 +4,10 @@
 #include <cassert>
 #include <cstddef>
 
+// hardware won't ever set the top three bits (5, 6 ,7), so we assert against them in requestInterrupt and clearFlagBit
+// Here, we mask against them, query should not raise asserts
+// see: https://gbdev.io/pandocs/Interrupts.html - https://gbdev.io/pandocs/Interrupt_Sources.html
+
 std::optional<int> InterruptController::queryPendingInterrupt() const {
   std::byte pending_mask{interrupt_enable_ & interrupt_flag_ & std::byte{0x1F}};
 
@@ -15,6 +19,7 @@ std::optional<int> InterruptController::queryPendingInterrupt() const {
   return std::countr_zero(std::to_integer<uint8_t>(pending_mask));
 }
 
+//
 void InterruptController::requestInterrupt(uint8_t bit) {
   assert(bit < 5 &&
          "InterruptController::requestInterrupt-> bit with value greater than 4 requested an "
@@ -23,7 +28,6 @@ void InterruptController::requestInterrupt(uint8_t bit) {
 }
 
 void InterruptController::clearFlagBit(uint8_t bit) {
-  assert(bit < 5 &&
-         "InterruptController::clearFlagBit -> bit with value greater than 4 passed to clear");
+  assert(bit < 5 && "InterruptController::clearFlagBit -> bit with value greater than 4 passed to clear");
   interrupt_flag_ &= ~(std::byte{1} << bit);
 }
