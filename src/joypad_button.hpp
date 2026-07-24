b77d4f038e66e6ac7e409bac68a32ee475b0b3e0
Rob <robbie.mcl@icloud.com>
2026-07-24 15:27:35 -0500

    finishes joypad tests

--- src/joypad_button.hpp
+++ src/joypad_button.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <cstdint>
+#include <type_traits>
 
 // enum used as both identifiers for button conditionals and bit positions of buttons in Joypad::joypad_buttons_
 namespace joypad {
   enum class Button : uint8_t { kRight = 0, kLeft, kUp, kDown, kA, kB, kSelect, kStart };
-}
+
+  constexpr uint8_t operator<<(int lhs, Button rhs) { return static_cast<uint8_t>(lhs << static_cast<uint8_t>(rhs)); }
+}  // namespace joypad
